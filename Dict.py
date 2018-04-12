82734b58a597b4754a3af1d2114a83d01b17f5ea
ellenedaniel <35457058+ellenedaniel@users.noreply.github.com>
2018-04-13 04:59:44 +0800

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,21 @@
+import collections
+people = {1: {'name': 'Marivic', 'age': '32', 'sex': 'Female'},
+          2: {'name': 'Mich', 'age': '23', 'sex': 'Female'},
+          3: {'name': 'JP', 'age': '25', 'sex': 'Male',},
+          4: {'name': 'John', 'age': '29', 'sex': 'Male', }}
+          
+print(people[1]['name'])
+print(people[1]['age'])
+print(people[1]['sex'])
+
+print(people[2]['name'])
+print(people[2]['age'])
+print(people[2]['sex'])
+
+print(people[3]['name'])
+print(people[3]['age'])
+print(people[3]['sex'])
+
+print(people[4]['name'])
+print(people[4]['age'])
+print(people[4]['sex'])
